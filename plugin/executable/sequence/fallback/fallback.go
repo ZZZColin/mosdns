24237290307ce4b95a07c23e062a809285251ab8
Colin Zhao <49432881+ZZZColin@users.noreply.github.com>
2026-09-21 15:30:44 +0800

    Change context for deadline in fallback.go

--- plugin/executable/sequence/fallback/fallback.go
+++ plugin/executable/sequence/fallback/fallback.go
@@ -218,5 +218,5 @@ func makeDdlCtx(ctx context.Context, timeout time.Duration) (context.Context, fu
 	if !ok {
 		ddl = time.Now().Add(timeout)
 	}
-	return context.WithDeadline(context.Background(), ddl)
+	return context.WithDeadline(ctx, ddl)
 }
